3933826ff31b22bc2dec0b2b1a6065ae4b6e43b9
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-11 09:19:49 +1100

    Added bootstrap and updated main menu link

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss');
+
 Vue.config.productionTip = false;
 
 new Vue({
