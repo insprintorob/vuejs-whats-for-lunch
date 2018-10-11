76d9763099ac9053103db6f38da3421b60ec0906
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-11 16:13:44 +1100

    Unit test for WhatsForLunch component

---
+++ tests/unit/whats-for-lunch-component.spec.ts
@@ -0,0 +1,51 @@
+import { shallowMount } from '@vue/test-utils';
+import WhatsForLunch from '@/components/WhatsForLunch.vue';
+
+let mockRecipesResponse = {
+    "recipes": [
+        {
+            "title": "Ham and Cheese Toastie",
+            "ingredients": ["Ham", "Cheese", "Bread", "Butter"]
+        },
+        {
+            "title": "Fry-up",
+            "ingredients": ["Bacon", "Eggs", "Baked Beans", "Mushrooms", "Sausage", "Bread"]
+        }
+    ]
+};
+
+let mockIngredientsResponse = {
+    "ingredients": [
+        {
+            "title": "Ham",
+            "best-before": "2018-10-20",
+            "use-by": "2018-10-25"
+        },
+        {
+            "title": "Cheese",
+            "best-before": "2018-10-20",
+            "use-by": "2018-10-25"
+        }
+    ]
+};
+
+jest.mock('node-fetch', () => {
+    return (url : string) => {
+        return {
+            json : () => {
+                switch(url) {
+                    case 'recipes.json':
+                        return mockRecipesResponse;
+                    case 'ingredients.json':
+                        return mockIngredientsResponse;
+                }
+            }
+        }
+    }
+});
+
+describe('WhatsForLunch.vue', () => {
+  it('renders whats for lunch wihout errors', () => {
+    shallowMount(WhatsForLunch, {}); // Will trigger whatsForLunch();
+  });
+});
