3fc36d954e2a65d4b19b890b929f34a6ce447d07
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-11 16:17:27 +1100

    Updated indentation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <div id="app" class="container">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/whats-for-lunch">Whats For Lunch</router-link>
+    <div id="app" class="container">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/whats-for-lunch">Whats For Lunch</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
 #nav {
-  padding-bottom: 10px;
+    padding-bottom: 10px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/components/WhatsForLunch.vue
+++ src/components/WhatsForLunch.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <h1>Heres whats for lunch</h1>
-      <ul id="recipeList">
-        <li v-for="recipe in whatsForLunch">
-          {{ recipe.title }}
-        </li>
-      </ul>
+    <div>
+        <h1>Heres whats for lunch</h1>
+            <ul id="recipeList">
+                <li v-for="recipe in whatsForLunch">
+                    {{ recipe.title }}
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -16,18 +16,18 @@ import { whatsForLunch } from '../whats-for-lunch';
 
 @Component
 export default class WhatsForLunch extends Vue {
-  /**
-   * A list of recipes that are good to eat
-   * @var Array
-   */
-  whatsForLunch : Array<any> = [];
+    /**
+     * A list of recipes that are good to eat
+     * @var Array
+     */
+    whatsForLunch : Array<any> = [];
 
-  /**
-   * Initialise the whatsForLunch array on component creation
-   */
-  async created() : Promise<void> {
-    this.whatsForLunch = await whatsForLunch();
-    return Promise.resolve();
-  }
+    /**
+     * Initialise the whatsForLunch array on component creation
+     */
+    async created() : Promise<void> {
+        this.whatsForLunch = await whatsForLunch();
+        return Promise.resolve();
+    }
 }
 </script>
